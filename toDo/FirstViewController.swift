23d3e73814657193b8c75aa8e24d8959f9111170
Paul Kim <pkayokay@gmail.com>
2016-11-29 23:37:21 -0500

    cell functions

--- toDo/FirstViewController.swift
+++ toDo/FirstViewController.swift
@@ -14,6 +14,22 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     @IBOutlet weak var myTableView: UITableView!
     
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+       return (list.count)
+    }
+
+    
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
